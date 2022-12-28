cfddc0e431bce038dc9d6dc3f4f18ab764126b18
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2022-12-28 16:30:01 +0100

    changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
 
 <template>
   <Frame>
-    <Page actionBarHidden="true" class="bg-white">
+    <Page actionBarHidden="true">
       <ScrollView>
         <StackLayout>
           <GridLayout rows="* auto auto 100">
@@ -17,54 +17,67 @@
               row="1"
               verticalAlignment="bottom"
               text="Velkommen Tilbage!"
-              class="text-3xl text-white px-12"
+              class="text-3xl text-white px-5"
             ></Label>
             <Label
               row="2"
               verticalAlignment="top"
               text="pil ved mig moussa"
-              class="text-lg text-white px-12"
+              class="text-lg text-white px-5"
             ></Label>
           </GridLayout>
-          
-          <ScrollView>
-          <Label
-            class="font-bold text-lg mx-4 mb-2 text-gray-300"
-            text="Email"
-          ></Label>
-          <TextField
-            class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md"
-          />
-        </ScrollView>
 
-          <Label
-            class="font-bold text-lg mx-4 mb-2 text-gray-300 mt-3"
-            text="Email"
-          ></Label>
-          <TextField
-            secure="true"
-            class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md"
-          />
+          <ScrollView class="mb-4">
+            <GridLayout rows="auto auto auto auto">
+              <Label
+                class="font-bold text-lg mx-4 mb-2 dark:text-gray-300 text-gray-600"
+                text="Email"
+                row="0"
+              ></Label>
+              <TextField
+                row="1"
+                class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md dark:bg-gray-900 dark:text-white"
+              />
+
+              <Label
+                class="font-bold text-lg mx-4 mb-2 dark:text-gray-300 text-gray-600 mt-3"
+                text="Kodeord"
+                row="2"
+              ></Label>
+              <TextField
+                row="3"
+                secure="true"
+                class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md dark:bg-gray-900 dark:text-white"
+              />
+            </GridLayout>
+          </ScrollView>
 
           <!-- Everything else inside the scroll view -->
           <Button
-            class="my-4 px-4 py-2 dark:text-black bg-white border-2 border-blue-400 rounded-lg"
+            class="py-4 mx-4 d bg-blue-500 text-white rounded-lg"
           >
-            View Details
+            Log ind
           </Button>
-          <GridLayout columns="* auto *">
-            <StackLayout class="bg-gray-300 h-1 mx-4"></StackLayout>
-            <StackLayout class="bg-gray-300 h-1 mx-4" col="2"></StackLayout>
+          <GridLayout class="py-3" columns="* auto *">
+            <StackLayout class="bg-gray-300 h-[5px] mx-4"></StackLayout>
+            <StackLayout class="bg-gray-300 h-[5px] mx-4" col="2"></StackLayout>
 
             <Label col="1" text="Eller" class="mx-4 text-gray-400"></Label>
           </GridLayout>
           <Button
-            class="my-4 px-4 py-2 dark:text-black bg-white border-2 border-blue-400 rounded-lg"
+            class="py-4 mx-4 bg-transparent dark:text-gray-300 border-4 text-gray-500 border-gray-500 rounded-lg"
           >
-            View Details
+            Register
           </Button>
         </StackLayout>
       </ScrollView>
     </Page>
   </Frame>
 </template>
+
+<style>
+Button {
+  android-elevation: 0;
+  android-dynamic-elevation-offset: 0;
+}
+</style>
