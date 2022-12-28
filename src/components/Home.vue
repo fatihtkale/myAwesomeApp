f1834c88f6b49b0db17d1f3695c44a754b76db86
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2022-12-28 06:50:17 +0100

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,8 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <Frame>
-    <Page actionBarHidden="true">
+    <Page actionBarHidden="true" class="bg-white">
       <ScrollView>
         <StackLayout>
           <GridLayout rows="* auto auto 100">
@@ -27,28 +26,29 @@
               class="text-lg text-white px-12"
             ></Label>
           </GridLayout>
-
+          
+          <ScrollView>
           <Label
             class="font-bold text-lg mx-4 mb-2 text-gray-300"
             text="Email"
           ></Label>
           <TextField
             class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md"
-            hint="Enter text..."
+          />
+        </ScrollView>
+
+          <Label
+            class="font-bold text-lg mx-4 mb-2 text-gray-300 mt-3"
+            text="Email"
+          ></Label>
+          <TextField
+            secure="true"
+            class="py-4 text-black px-4 mx-4 bg-gray-100 border-none rounded-md"
           />
 
           <!-- Everything else inside the scroll view -->
           <Button
-            class="
-              my-4
-              px-4
-              py-2
-              dark:text-black
-              bg-white
-              border-2 border-blue-400
-              rounded-lg
-            "
-            horizontalAlignment="center"
+            class="my-4 px-4 py-2 dark:text-black bg-white border-2 border-blue-400 rounded-lg"
           >
             View Details
           </Button>
@@ -56,19 +56,10 @@
             <StackLayout class="bg-gray-300 h-1 mx-4"></StackLayout>
             <StackLayout class="bg-gray-300 h-1 mx-4" col="2"></StackLayout>
 
-            <Label col="1" text="Eller" class="mx-4"></Label>
+            <Label col="1" text="Eller" class="mx-4 text-gray-400"></Label>
           </GridLayout>
           <Button
-            class="
-              my-4
-              px-4
-              py-2
-              dark:text-black
-              bg-white
-              border-2 border-blue-400
-              rounded-lg
-            "
-            horizontalAlignment="center"
+            class="my-4 px-4 py-2 dark:text-black bg-white border-2 border-blue-400 rounded-lg"
           >
             View Details
           </Button>
@@ -77,9 +68,3 @@
     </Page>
   </Frame>
 </template>
-
-<style>
-/* .info {
-    font-size: 20;
-  } */
-</style>
