b153156aa82b0118e573a94a4d76a121a210d82e
Fatih Toprakkale <91729123+fto-exerp@users.noreply.github.com>
2023-01-17 10:28:13 +0100

    bottom nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,87 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, toRaw } from "nativescript-vue";
+
+const selected = ref(0);
+
+const navigate = (selectedIndex: number) => {
+  if (selected.value === selectedIndex) return;
+  selected.value = selectedIndex;
+};
+</script>
 
 <template>
   <Page actionBarHidden="true">
-    <StackLayout>
-      <Label text="hello"></Label>
-      <Label text="hello2"></Label>
-    </StackLayout>
+    <GridLayout rows="*,auto">
+      <StackLayout row="0">
+        <Label :text="selected"></Label>
+      </StackLayout>
+      <FlexboxLayout
+        row="1"
+        flexDirection="row"
+        justifyContent="space-around"
+        class="py-3 bg-green-500"
+      >
+        <StackLayout col="0">
+          <FlexboxLayout
+            @tap="navigate(0)"
+            :class="selected == 0 ? 'bg-green-300 rounded-full px-4' : ''"
+            class="flex items-center justify-center py-2 transition-all ease-in-out delay-150"
+          >
+            <Image col="0" width="30" src="~/assets/task.png"></Image>
+            <Label
+              v-if="selected === 0"
+              class="text-green-600 ml-1 font-bold text-[15px]"
+              col="1"
+              text="Opgaver"
+            ></Label>
+          </FlexboxLayout>
+        </StackLayout>
+        <StackLayout col="1">
+          <FlexboxLayout
+            @tap="navigate(1)"
+            :class="selected == 1 ? 'bg-green-300 rounded-full px-4' : ''"
+            class="flex items-center justify-center py-2 transition-all ease-in-out delay-150"
+          >
+            <Image col="0" width="30" src="~/assets/time.png"></Image>
+            <Label
+              v-if="selected === 1"
+              class="text-green-600 ml-1 font-bold text-[15px]"
+              col="1"
+              text="Tidsregistrering"
+            ></Label>
+          </FlexboxLayout>
+        </StackLayout>
+        <StackLayout col="2">
+          <FlexboxLayout
+            @tap="navigate(2)"
+            :class="selected == 2 ? 'bg-green-300 rounded-full px-4' : ''"
+            class="flex items-center justify-center py-2 transition-all ease-in-out delay-150"
+          >
+            <Image col="0" width="30" src="~/assets/settings.png"></Image>
+            <Label
+              v-if="selected === 2"
+              class="text-green-600 ml-1 font-bold text-[15px]"
+              col="1"
+              text="Indstillinger"
+            ></Label>
+          </FlexboxLayout>
+        </StackLayout>
+        <StackLayout col="3">
+          <FlexboxLayout
+            @tap="navigate(3)"
+            :class="selected == 3 ? 'bg-green-300 rounded-full px-4' : ''"
+            class="flex items-center justify-center py-2 transition-all ease-in-out delay-150"
+          >
+            <Image col="0" width="30" src="~/assets/messages.png"></Image>
+              <Label
+              v-if="selected === 3"
+              class="text-green-600 ml-1 font-bold text-[15px]"
+              col="1"
+              text="Beskeder"
+              ></Label>
+          </FlexboxLayout>
+        </StackLayout>
+      </FlexboxLayout>
+    </GridLayout>
   </Page>
 </template>
